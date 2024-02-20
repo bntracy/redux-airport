3c342432e9b1e617fe3ab498da006a77dce45cdc
Beth Tracy <bntracy@gmail.com>
2024-02-20 16:01:09 -0600

    boilerplate added to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,18 +4,28 @@ import App from './components/App/App';
 
 /** TODO: import REDUX **/
 
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 /** TODO: Add REDUCERS */
+
+const airlines = (state =[], action) => {
+    return state;
+}
 
 
 /** TODO: Create store */
 
+const storeInstance = createStore(airlines, applyMiddleware(logger));
 
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
